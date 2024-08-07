0dc6adac716b21c4ca5a8c643a5c0d15394ceca1
caioramineli <caiofrancoramineli3@gmail.com>
2024-08-07 18:13:13 -0300

    feat: add Header and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
+import { Input } from "./_components/ui/input"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button>Teste</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Caio!</h2>
+        <p>Quarta-Feira, 07 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="banner"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
